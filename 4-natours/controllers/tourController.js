a3e1b6ef5c4acfa12c8003dfebcb6cf98398f066
iamrohit <rohit@gmail.com>
2022-10-22 16:16:47 +0530

    implemented pagination

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -82,6 +82,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    /// V. Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('Sorry, this page does not exist');
+    }
+
+    query = query.skip(skip).limit(limit);
+
     /// 2. EXECUTE QUERY
     const tours = await query;
 
