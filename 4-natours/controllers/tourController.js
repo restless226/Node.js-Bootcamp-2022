5c0f8247e00a2471ed461e45977ec2395c4319c4
iamrohit <rohit@gmail.com>
2022-10-22 16:03:04 +0530

    implemented limiting fields

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -74,6 +74,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    /// IV. Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     /// 2. EXECUTE QUERY
     const tours = await query;
 
