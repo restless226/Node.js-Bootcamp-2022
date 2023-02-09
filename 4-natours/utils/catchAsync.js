c37e7ec174f97de7a84d054205797299f7b5df0e
restless226 <rapendse2002@gmail.com>
2023-02-09 19:58:13 +0530

    catch asynchronous errors

--- 4-natours/utils/catchAsync.js
+++ 4-natours/utils/catchAsync.js
@@ -1,6 +1,12 @@
 // to catch asynchronous errors
+module.exports = (fn) => (req, res, next) => {
+  fn(req, res, next).catch((err) => next(err));
+};
+
+/*
 module.exports = fn => {
   return (req, res, next) => {
     fn(req, res, next).catch(err => next(err));
   }
 };
+*/
