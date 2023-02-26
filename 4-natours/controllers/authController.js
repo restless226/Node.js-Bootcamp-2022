3bcda579307896619df6ee419cb8ebf3d51172c3
restless226 <rapendse2002@gmail.com>
2023-02-26 18:55:16 +0530

    signToken

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -3,6 +3,12 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
+const signToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
@@ -10,9 +16,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
   res.status(201).json({
     status: 'success',
     token,
@@ -34,14 +38,13 @@ exports.login = catchAsync(async (req, res, next) => {
 
   /// 2] Check if user exist and password is correct
   const user = await User.findOne({ email: email }).select('+password');
-  const isPasswordVerified = await User.verifyPassword(password, user.password);
-  if (!user || !isPasswordVerified) {
+  if (!user || !(await user.verifyPassword(password, user.password))) {
     return next(new AppError('Invalid email or password!', 401));
   }
   console.log('exports.login user = ', user);
 
   /// 3] If everything is ok, send json web token to the client
-  const token = '';
+  const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token,
