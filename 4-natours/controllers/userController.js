a9c8ebaeb1bdfcf0001e8e405c12f9a3357752ea
restless226 <rapendse2002@gmail.com>
2023-02-26 16:24:14 +0530

    getAllUsers

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -1,9 +1,18 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllUsers = catchAsync(async(req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -29,6 +29,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password!'],
     minLength: 8,
+    select: false,
     // trim: false,
   },
   passwordConfirm: {
