b94e31cfa88f6340e1d1b196d29ed4e4c31c1dba
restless226 <rapendse2002@gmail.com>
2023-02-26 10:27:19 +0530

    password hashing using bcryptjs

--- 4-natours/models/userModel.js
+++ 4-natours/models/userModel.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Please provide your name!'],
-    unique: true,
-    trim: true,
-    maxLength: [40, 'username must have less than or equal to 40 characters'],
+    // trim: true,
+    // maxLength: [40, 'username must have less than or equal to 40 characters'],
+    // unique: true,
     // minLength: [
     //   10,
     //   'username must have greater than or equal to 10 characters',
@@ -17,9 +18,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide your email!'],
     unique: true,
-    trim: false,
     lowercase: true,
     validate: [validator.isEmail, 'Please provide a valid email!'],
+    // trim: false,
   },
   photo: {
     type: String,
@@ -28,12 +29,30 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password!'],
     minLength: 8,
-    trim: false,
+    // trim: false,
   },
   passwordConfirm: {
+    // trim: false,
     type: String,
     required: [true, 'Please confirm your password!'],
+    validate: {
+      // This works only on CREATE and SAVE!!!
+      validator: function (e) {
+        return e === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function password was actually modified
+  if (!this.isModified('password')) return next();
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete the passwordConfirm field 
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
