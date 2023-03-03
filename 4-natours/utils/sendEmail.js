1e1b26abc2b01b5c918f9da59d115d7d5896310b
restless226 <rapendse2002@gmail.com>
2023-03-03 16:31:00 +0530

    nodemailer version changed to fix 500 error in forgotPassword

--- 4-natours/utils/sendEmail.js
+++ 4-natours/utils/sendEmail.js
@@ -5,6 +5,8 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: false,
+    logger: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
