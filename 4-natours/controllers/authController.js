2a3a84e5f861e6d66367bb5e2f3c63b9cdf8ca8d
restless226 <rapendse2002@gmail.com>
2023-03-05 11:50:17 +0530

    resetPassword

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,8 +1,9 @@
+const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
+const util = require('util');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
-const util = require('util');
 const sendEmail = require('./../utils/sendEmail');
 
 /// generates a unique jwt token
@@ -167,4 +168,36 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   }
 });
 
-exports.resetPassword = (req, res, next) => {};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  console.log('inside resetPassword in authController.js');
+
+  /// 1] Get user based on the token
+  const hashedToken = await crypto
+    .createHash('sha256')
+    .update(req.params.resetToken)
+    .digest('hex');
+  const user = User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetExpires: { $gt: Date.now() },
+  });
+
+  /// 2] If token has not expired and there is a user set the new password
+  if (!user) {
+    return next(new AppError('Token is invalid or has expired!', 400));
+  }
+  console.log({user}, {req});
+  user.password = req.body.password;
+  user.passwordConfirm = req.body.passwordConfirm;
+  user.passwordResetToken = undefined;
+  user.passwordResetExpires = undefined;
+  await user.save();
+
+  /// 3] Update changedPasswordAt property for the current user
+
+  /// 4] Log the user in and send JWT to the client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
