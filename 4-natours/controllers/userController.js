02d43a558d4764c6d1e56ea449a1a682ea10128c
restless226 <rapendse2002@gmail.com>
2023-03-08 23:52:44 +0530

    deleteMe

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -62,6 +62,14 @@ exports.getUser = (req, res) => {
   });
 };
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 exports.createUser = (req, res) => {
   console.log('inside createUser in userController.js');
   res.status(500).json({
