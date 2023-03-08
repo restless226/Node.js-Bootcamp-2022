f8e51f43864064479943a1f4b341b5455c06fadc
restless226 <rapendse2002@gmail.com>
2023-03-08 23:42:48 +0530

    updateMe

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -2,6 +2,16 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
+const generateFilteredBody = (body, ...allowedFields) => {
+  const filteredBody = {};
+  Object.keys(body).forEach((e) => {
+    if (allowedFields.includes(e)) {
+      filteredBody[e] = body[e];
+    }
+  });
+  return filteredBody;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   console.log('inside getAllUsers in userController.js');
   const users = await User.find();
@@ -12,6 +22,35 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     data: {
       users,
     },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log('inside updateMe in userController.js');
+
+  // 1] Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not defined for password updates. Please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  // 2] Filter out unwanted field names that are not allowed to be updated
+  const filteredBody = generateFilteredBody(req.body, 'name', 'email');
+
+  // 3] Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  // 4] send back success response
+  res.status(200).json({
+    status: 'success',
+    data: { user: updatedUser },
   });
 });
 
