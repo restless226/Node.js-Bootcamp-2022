9ecc3ffbb899325295538244912e3c02767079ed
restless226 <rapendse2002@gmail.com>
2023-03-14 11:02:26 +0530

    Review model

---
+++ 4-natours/models/reviewModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Tour = require('./tourModel');
+const User = require('./userModel');
+
+const reviewSchema = new mongoose.Schema({
+  review: {
+    type: String,
+    required: [true, 'A review must be non-empty!'],
+    maxLength: [
+      1000,
+      'A review must have less than or equal to 1000 characters',
+    ],
+    minLength: [
+      10,
+      'A review must have greater than or equal to 10 characters',
+    ],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
