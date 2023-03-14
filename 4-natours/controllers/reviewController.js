e14eb6ac0dc785dba1823db84417bd5282dc2c60
restless226 <rapendse2002@gmail.com>
2023-03-14 12:43:29 +0530

    reviewController

---
+++ 4-natours/controllers/reviewController.js
@@ -0,0 +1,27 @@
+const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+  console.log('inside getAllReviews in reviewController.js');
+  const reviews = await Review.find();
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: reviews.length,
+    data: {
+      reviews,
+    },
+  });
+});
+
+exports.createReview = catchAsync(async (req, res, next) => {
+  console.log('inside createReview in reviewController.js');
+  const newReview = await Review.create(req.body);
+  res.status(201).json({
+    status: 'success',
+    data: {
+      review: newReview,
+    },
+  });
+});
