def2315a44796c8b6caf59a8e85072f8dc6dd07f
restless226 <rapendse2002@gmail.com>
2023-03-14 11:07:45 +0530

    parent referencing reviews

--- 4-natours/models/reviewModel.js
+++ 4-natours/models/reviewModel.js
@@ -2,31 +2,47 @@ const mongoose = require('mongoose');
 const Tour = require('./tourModel');
 const User = require('./userModel');
 
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'A review must be non-empty!'],
-    maxLength: [
-      1000,
-      'A review must have less than or equal to 1000 characters',
-    ],
-    minLength: [
-      10,
-      'A review must have greater than or equal to 10 characters',
-    ],
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'A review must be non-empty!'],
+      maxLength: [
+        1000,
+        'A review must have less than or equal to 1000 characters',
+      ],
+      minLength: [
+        10,
+        'A review must have greater than or equal to 10 characters',
+      ],
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user.'],
+    },
   },
-  rating: {
-    type: Number,
-    default: 4.5,
-    min: [1, 'Rating must be above 1.0'],
-    max: [5, 'Rating must be below 5.0'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 const Review = mongoose.model('Review', reviewSchema);
 
