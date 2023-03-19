349cc6dd0ba6f68ea7ddffb8a47e2ce55950923b
restless226 <rapendse2002@gmail.com>
2023-03-19 21:06:42 +0530

    importing new json data

--- 4-natours/dev-data/data/import-dev-data.js
+++ 4-natours/dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -22,11 +24,17 @@ mongoose
 
 // SCRIPT TO READ JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!!!');
   } catch (err) {
     console.log(`importData error = ${err}`);
@@ -38,9 +46,11 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
-    console.log('Data successfully loaded!!!');
+    await User.deleteMany();
+    await Review.deleteMany();
+    console.log('Data successfully deleted!!!');
   } catch (err) {
-    console.log(`importData error = ${err}`);
+    console.log(`deleteData error = ${err}`);
   }
   process.exit();
 };
--- 4-natours/server.js
+++ 4-natours/server.js
@@ -32,21 +32,6 @@ mongoose
     process.exit(-1);
   });
 
-// console.log(app.get('env'));
-// console.log(process.env);
-// const testTour = new Tour({
-//   name: 'The Park Camper',
-//   price: 997,
-// });
-// testTour
-//   .save()
-//   .then((doc) => {
-//     console.log(doc);
-//   })
-//   .catch((err) => {
-//     console.log('ERROR!!!', err);
-//   });
-
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`server listening on port ${port}...`);
