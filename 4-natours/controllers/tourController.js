5d237eed60fbe98c72b2145b6b868605201b9cf5
restless226 <rapendse2002@gmail.com>
2023-03-19 17:14:33 +0530

    deleteOne factory function

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -2,6 +2,7 @@ const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
 
 exports.aliasTopTours = async (req, res, next) => {
   console.log('inside aliasTopTours in tourController.js');
@@ -74,17 +75,8 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  console.log('inside deleteTour in tourController.js');
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+
+exports.deleteTour = factory.deleteOne(Tour);
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   console.log('inside getTourStats in tourController.js');
