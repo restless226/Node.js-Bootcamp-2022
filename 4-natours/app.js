c5ccc7a31fad620dca61733ff81e85e8d8b02fcd
restless226 <rapendse2002@gmail.com>
2023-03-24 23:10:29 +0530

    setting up pug template

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,15 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 /// 1] GLOBAL MIDDLEWARES
+
+// Serving static files
+// app.use(express.static(`${__dirname}/public`));
+app.set('views', path.join(__dirname, 'public'));
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -62,9 +71,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -75,6 +81,9 @@ app.use((req, res, next) => {
 /// 2] ROUTES
 
 /// mouting the routers
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
