e19fc9c8102a6290c5121b9ecbbf7478a3ac59c4
restless226 <rapendse2002@gmail.com>
2023-03-29 10:40:57 +0530

    cookies

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -79,8 +79,10 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
-  // console.log('token = ', token);
+  console.log({ token });
   if (!token) {
     return next(
       new AppError(
@@ -126,6 +128,38 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+// Only for rendered pages, no errors!
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      console.log("isLoggedIn error = ", err);
+      return next();
+    }
+  }
+  next();
+};
 
 exports.authorize = (...roles) => {
   console.log('inside authorize in authController.js');
