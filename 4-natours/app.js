35bf6517624d3cf756610b04a3fa9427a3f40697
restless226 <rapendse2002@gmail.com>
2023-03-29 09:05:39 +0530

    cookieParser

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -69,6 +70,7 @@ app.use('/api', limiter);
 
 // Body parser - reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(cookieParser());
 
 // Data sanitization against NoSQL query injection (XSS)
 app.use(mongoSanitize());
@@ -94,7 +96,8 @@ app.use(
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log('app.use middleware req.headers = ', req.headers);
+  const headers = req.headers, cookies = req.cookies;
+  console.log({headers}, {cookies});
   next();
 });
 
