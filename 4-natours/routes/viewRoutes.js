58972864382a4bc8381a27f43a9c330661f2138c
restless226 <rapendse2002@gmail.com>
2023-04-04 22:46:13 +0530

    getMyTours

--- 4-natours/routes/viewRoutes.js
+++ 4-natours/routes/viewRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
@@ -10,6 +11,12 @@ router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get(
+  '/my-tours',
+  bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours
+);
 
 router.post(
   '/submit-user-data',
